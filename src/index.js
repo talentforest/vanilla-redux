656ef0329a934f04b261d3fb6c772e85e6db10b9
talentforest <talentforest0501@gmail.com>
2022-01-18 01:49:29 +0900

    clean

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 바닐라 JS로 만들 것이기 때문에 다 삭제
