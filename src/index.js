51838b7d6f16ab273f2f7b016fdfae615edfc3d3
talentforest <talentforest0501@gmail.com>
2022-01-18 03:23:10 +0900

    Create Counter with Redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// 바닐라 JS로 만들 것이기 때문에 다 삭제
+import { createStore } from 'redux';
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const ADD = "ADD"
+const MINUS = "MINUS"
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({type: ADD}))
+minus.addEventListener("click", () => countStore.dispatch({type: MINUS}))
