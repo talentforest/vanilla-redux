d909372c7cd07158e13cb60022d2ecc8fb0fae46
talentforest <talentforest0501@gmail.com>
2022-01-19 17:16:49 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,22 +1,17 @@
 import {createStore} from "redux";
-import {createAction} from "@reduxjs/toolkit"
+import {createAction, createReducer} from "@reduxjs/toolkit"
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-console.log(addToDo())
-
-const reducer = (state = [], action) => {
-  switch(action.type) {
-    case addToDo.type:
-      return [...state, {text: action.payload, id: Date.now()}];
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
-
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({text: action.payload, id: Date.now()});
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter(toDo => toDo.id !== action.payload)
+})
+ 
 const store = createStore(reducer);
 
 export const actionCreators = {
